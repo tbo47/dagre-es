tbo47/dagre-es

commit 60d4b25b0c8dc689c108addb19e03f4f4534c754

refactor: prevent import mutation causing build issues with some bundlers

src/dagre-js/create-edge-labels.js
@@ -3,9 +3,9 @@ import _ from 'lodash-es';
 import { addLabel } from "./label/add-label";
 import * as util from "./util";
 
-export { createEdgeLabels };
+export { createEdgeLabels, setCreateEdgeLabels };
 
-function createEdgeLabels(selection, g) {
+var createEdgeLabels = function(selection, g) {
   var svgEdgeLabels = selection.selectAll("g.edgeLabel")
     .data(g.edges(), function (e) { return util.edgeToId(e); })
     .classed("update", true);
@@ -42,4 +42,8 @@ function createEdgeLabels(selection, g) {
     .remove();
 
   return svgEdgeLabels;
+}
+
+function setCreateEdgeLabels(value) {
+  setCreateEdgeLabels = value;
 }

src/dagre-js/arrows.d.ts
@@ -1,3 +1,2 @@
-export function normal(parent: any, id: any, edge: any, type: any): void;
-export function vee(parent: any, id: any, edge: any, type: any): void;
-export function undirected(parent: any, id: any, edge: any, type: any): void;
+export var arrows: any;
+export function setArrows(value: any): any;

src/dagre-js/arrows.js
@@ -1,9 +1,18 @@
 import * as util from "./util";
 
 export {
+  arrows,
+  setArrows,
+};
+
+var arrows = {
   normal,
   vee,
   undirected
+};
+
+function setArrows(value) {
+  arrows = value;
 };
 
 function normal(parent, id, edge, type) {

src/dagre-js/create-clusters.d.ts
@@ -1,1 +1,2 @@
 export function createClusters(selection: any, g: any): any;
+export function setCreateClusters(value: any): any;

src/dagre-js/create-clusters.js
@@ -2,9 +2,9 @@ import * as d3 from "d3";
 import { addLabel } from "./label/add-label";
 import * as util from "./util";
 
-export { createClusters };
+export { createClusters, setCreateClusters };
 
-function createClusters(selection, g) {
+var createClusters = function (selection, g) {
   var clusters = g.nodes().filter(function (v) { return util.isSubgraph(g, v); });
   var svgClusters = selection.selectAll("g.cluster")
     .data(clusters, function (v) { return v; });
@@ -40,4 +40,8 @@ function createClusters(selection, g) {
   });
 
   return svgClusters;
+}
+
+function setCreateClusters(value) {
+  createClusters = value;
 }

src/dagre-js/create-edge-labels.d.ts
@@ -1,1 +1,2 @@
 export function createEdgeLabels(selection: any, g: any): any;
+export function setCreateEdgeLabels(value: any): any;

src/dagre-js/create-edge-paths.d.ts
@@ -1,1 +1,2 @@
 export function createEdgePaths(selection: any, g: any, arrows: any): any;
+export function setCreateEdgePaths(value: any): any;

src/dagre-js/create-edge-paths.js
@@ -3,9 +3,9 @@ import _ from 'lodash-es';
 import { intersectNode } from "./intersect/intersect-node";
 import * as util from "./util";
 
-export { createEdgePaths };
+export { createEdgePaths, setCreateEdgePaths };
 
-function createEdgePaths(selection, g, arrows) {
+var createEdgePaths = function(selection, g, arrows) {
   var previousPaths = selection.selectAll("g.edgePath")
     .data(g.edges(), function (e) { return util.edgeToId(e); })
     .classed("update", true);
@@ -57,6 +57,10 @@ function createEdgePaths(selection, g, arrows) {
     });
 
   return svgPaths;
+}
+
+function setCreateEdgePaths(value) {
+  createEdgePaths = value;
 }
 
 function makeFragmentRef(url, fragmentId) {

commit cbccb4f73edc7110f4ccd717e1f9b71972e7fda4

fix: fix bug in setCreateEdgeLabels
This function was actually overwriting itself, not createEdgeLabels.
This was caught by eslint!

Fixes: 60d4b25b0c8dc689c108addb19e03f4f4534c754

src/dagre-js/create-edge-labels.js
@@ -5,7 +5,7 @@ import * as util from './util';
 
 export { createEdgeLabels, setCreateEdgeLabels };
 
-var createEdgeLabels = function (selection, g) {
+let createEdgeLabels = function (selection, g) {
   var svgEdgeLabels = selection
     .selectAll('g.edgeLabel')
     .data(g.edges(), function (e) {
@@ -50,5 +50,5 @@ var createEdgeLabels = function (selection, g) {
 };
 
 function setCreateEdgeLabels(value) {
-  setCreateEdgeLabels = value;
+  createEdgeLabels = value;
 }