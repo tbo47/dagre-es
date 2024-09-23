27f3f8d28a2a0df97c0709ce2cc350f353c36dae
tbo47 <thibaut.lassale@gmail.com>
2024-09-23 12:15:50 +0000

    defensive check

--- src/dagre/position/bk.js
+++ src/dagre/position/bk.js
@@ -71,7 +71,7 @@ function findType1Conflicts(g, layering) {
     return layer;
   }
 
-  if (Array.isArray(layering) && layering.length > 0) layering.reduce(visitLayer);
+  if (layering.length > 0) layering.reduce(visitLayer);
   return conflicts;
 }
 
@@ -114,7 +114,7 @@ function findType2Conflicts(g, layering) {
     return south;
   }
 
-  if (Array.isArray(layering) && layering.length > 0) layering.reduce(visitLayer);
+  if (layering.length > 0) layering.reduce(visitLayer);
   return conflicts;
 }
 
